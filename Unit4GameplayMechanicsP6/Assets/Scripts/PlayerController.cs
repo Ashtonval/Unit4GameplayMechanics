d3e5bf47fda6ef3fc751a7ff12de1598aab220a2
Ashtonval <tennisjedi20xx@gmail.com>
2023-09-07 13:53:36 -0700

    yes
    yes

--- Unit4GameplayMechanicsP6/Assets/Scripts/PlayerController.cs
+++ Unit4GameplayMechanicsP6/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float powerupStrength = 15.0f;
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
     public bool hasPowerup;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +23,36 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
     private void OnTriggerEnter(Collider other)
     {
        if (other.CompareTag("Powerup"))
+        {
             hasPowerup = true;
-        Destroy(other.gameObject);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }    
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("enemy") && hasPowerup)
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
             Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+            
     }
 }
